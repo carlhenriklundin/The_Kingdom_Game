fe104cc6d08d121c2cde3797846378b21b8a7b60
Carl-Henrik Lundin <lundin.carlhenrik@gmail.com>
2021-10-21 15:43:45 +0200

    Add project files.

---
+++ The_Kingdom_Game/PlayingCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Kingdom_Game
+{
+    class PlayingCard
+    {
+        int cardNumber { get; init; }
+        
+        int xLandscape { get; init; }
+        int xNumberOfCrown { get; init; }
+
+        int yLandscape { get; init; }
+        int yNumberOfCrown { get; init; }
+
+    }
+}
---
+++ The_Kingdom_Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_Kingdom_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
