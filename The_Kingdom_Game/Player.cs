ab2bb2b7ec44ca5d169e9982faf52342880bcfb6
Carl-Henrik Lundin <lundin.carlhenrik@gmail.com>
2021-10-22 07:47:57 +0200

    7;47

--- The_Kingdom_Game/Player.cs
+++ The_Kingdom_Game/Player.cs
@@ -8,31 +8,63 @@ namespace The_Kingdom_Game
 {
     class Player
     {
-        (Landscape?, int)[,] PlayerBoard = new (Landscape?, int)[9, 9];
-
-
-        public void InsertPlayingCard(PlayingCard playingCard)
+        public (Landscape landscape, int numberOfCrown)[,] Board = new (Landscape, int)[9, 9];
+        
+        public Player()
         {
-            Console.Write("Vilken rad ska högra spelkortet hamna i?");
-            int RightRad = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Vilken column ska högra spelkortet hamna i?");
-            int RIghtColumn = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Vilken rad ska vänstra spelkortet hamna i?");
-            int LeftRad = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Vilken column ska vänstra spelkortet hamna i?");
-            int LeftColumn = Convert.ToInt32(Console.ReadLine());
-
-            PlayerBoard[RightRad, RIghtColumn] = new (playingCard.rightSideLandscape, playingCard.rightSideNumberOfCrown);
-            PlayerBoard[LeftRad, LeftColumn] = new (playingCard.rightSideLandscape, playingCard.leftSideNumberOfCrown);
+            Board[4, 4] = (Landscape.Water, 0);
         }
+
+        public void InsertPlayingCard(PlayingCard card)
+        {
+            Console.Write($"Vilken sida av kortet {card.ToString()} vill du börja med att lägga ut (höger eller vänster?");
+            string input = Console.ReadLine();
+            if (input == "vänster") (card.rightSide, card.leftSide) = (card.leftSide, card.leftSide);
+
+            (int row, int column) firstSide = GetPosition(card.rightSide.landscape);
+            (int row, int column) secondSide = GetPosition(card.leftSide.landscape);
+
+            (Board[firstSide.row, firstSide.column], Board[secondSide.row, secondSide.column]) = (card.rightSide, card.leftSide);
+        }
+
+        public (int, int) GetPosition(Landscape landscape)
+        {
+            Console.Write($"I vilken rad ska spelkortet läggas in i?");
+            int rad = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"I vilken column ska spelkortet läggas in i?");
+            int column = Convert.ToInt32(Console.ReadLine());
+
+            CheckIfPositionIsLegal(rad,column, landscape, true);
+
+            return (rad, column);
+        }
+
+
+        public bool CheckIfPositionIsLegal(int row, int column, Landscape landscape, bool firstCard)
+        {
+            if (firstCard && (Board[row, column].landscape == Landscape.Empty) && (Board[row - 1, column].landscape == landscape || Board[row, column + 1].landscape == landscape || Board[row + 1, column].landscape == landscape || Board[row, column - 1].landscape == landscape))
+                
+            if (!firstCard && Board[row, column].landscape == Landscape.Empty)
+
+
+
+
+
+                return true;
+
+                return false;
+        }
+
+        
 
         public void PrintPLayingBoard()
         {
-            for (int rad = 0; rad < PlayerBoard.GetLength(0); rad++)
+            for (int rad = 0; rad < Board.GetLength(0); rad++)
             {
-                for (int column = 0; column < PlayerBoard.GetLength(1); column++)
+                for (int column = 0; column < Board.GetLength(1); column++)
                 {
-                    if (PlayerBoard[rad, column].Item1 != null) Console.Write($"{PlayerBoard[rad, column],-12}");
+                    Console.Write($"{Board[rad, column],-12}");
+                    if (Board[rad, column] == null) Console.Write("null");
                 }
                 Console.WriteLine();
                 Console.WriteLine();
@@ -42,3 +74,14 @@ namespace The_Kingdom_Game
         }
     }
 }
+
+/*
+           if (Board[position.row - 1, position.column] != null || Board[position.row, position.column + 1] != null || Board[position.row + 1, position.column] != null || Board[position.row, position.column - 1] != null)
+               {
+               int row, int column) position = AskingForPosition(false);
+
+               Board[RightRad, RIghtColumn] = new(playingCard.rightSideLandscape, playingCard.rightSideNumberOfCrown);
+               Board[LeftRad, LeftColumn] = new(playingCard.rightSideLandscape, playingCard.leftSideNumberOfCrown);
+           }
+           else Console.WriteLine("Kortet du lägger måste ha kontakt med ett annat kort!");
+           */
--- The_Kingdom_Game/PlayingCard.cs
+++ The_Kingdom_Game/PlayingCard.cs
@@ -6,32 +6,29 @@ using System.Threading.Tasks;
 
 namespace The_Kingdom_Game
 {
-    public enum Landscape {Water, Fields, Forest, Mine, Sand };
+    public enum Landscape {Empty, Null, Water, Fields, Forest, Mine, Sand, King };
     public class PlayingCard
     {
         public int cardNumber { get;}
 
-        public Landscape? rightSideLandscape { get; } = null;
-        public int rightSideNumberOfCrown {get;}
+        public (Landscape landscape, int numberOfCrown) rightSide { get;}
+        public (Landscape landscape, int numberOfCrown) leftSide { get;}
 
-        public Landscape? leftSideLandscape { get; } = null;
-        public int leftSideNumberOfCrown {get;}
 
         public PlayingCard(int cardNumber)
         {
             Random random = new Random();
             this.cardNumber = cardNumber;
-            rightSideLandscape = (Landscape)random.Next(0, 5);
-            leftSideLandscape = (Landscape)random.Next(0, 5);
+            rightSide = ((Landscape)random.Next(0, 5), 0);
+            leftSide = ((Landscape)random.Next(0, 5), 0);
 
             if (cardNumber > 17)
             {
-                rightSideNumberOfCrown = random.Next(0, 4);
-                leftSideNumberOfCrown = random.Next(0, 4);
+            rightSide = ((Landscape)random.Next(0, 5), random.Next(0, 4));
+            leftSide = ((Landscape)random.Next(0, 5), random.Next(0, 4));
             }
         }
-        public override string ToString() => $"{rightSideLandscape}({rightSideNumberOfCrown}){leftSideLandscape,20}({leftSideNumberOfCrown})";
-       
-
+        public override string ToString() => $"{rightSide,-20}{leftSide}";
+      
     }
 }
